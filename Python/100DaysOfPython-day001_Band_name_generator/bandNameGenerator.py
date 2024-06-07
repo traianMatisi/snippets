771b0d34e494c67a864943fb95d49f51fc36857f
Traian <traian@tutanota.com>
2024-06-07 19:01:44 -0300

    100 days of python, day 1

---
+++ Python/100DaysOfPython-day001_Band_name_generator/bandNameGenerator.py
@@ -0,0 +1,12 @@
+# Band name generator - 100day of python day 001 - 07-jun-2024
+# Author: Traian
+
+cityName = input("Tell me, please, the name of the city, district or place you grew up: ")
+
+petName = input("Now, tell me a cool name you have ever given to a pet: ")
+
+print(f"Option 1: {cityName}\'s {petName}.\nOption 2: {petName} {cityName}.")
+
+print("Wha'd'ya think??")
+
+
