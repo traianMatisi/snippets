4013adb8245f59ba2e904620f04f4cf5ed4c7dc4
Traian <traian@tutanota.com>
2024-06-18 16:27:02 -0300

    final hangman

--- Python/HDoP-d7e0-hangman.py
+++ Python/HDoP-d7e0-hangman.py
@@ -2,17 +2,19 @@
 # Author: Traian 15-6-24
 
 from random import choice
-import os
+#import os
 
 play = True
 words = ["zerozerozero", "oneoneone", "twotwotwo", "threethreethree", "fourfourfour", "fivefivefive", "sixsixsix", "sevensevenseven", "eighteighteight", "nineninenine", "tententen",]
 word = choice(words).upper()
+word = input("Type the secret word:\n==> ").upper().replace(" ", "")
 letters = []
+blanks = []
+guesses = []
 for i in word:
     letters.append(i)
-blank = []
 for i in letters:
-    blank.append('_')
+    blanks.append('_')
 hanger = [
 ["_","_","_","_","_","_","_","_","_"," ",],
 ["|","/"," "," "," "," "," "," ","|"," ",],
@@ -22,34 +24,52 @@ hanger = [
 ["|"," "," "," "," "," "," "," "," "," ",],
 ["|"," "," "," "," "," "," "," "," "," ",],
 ]
-
-# Head [2][8]
-# Torso [3][8]
-# Arms [3][7] and [3][9]
-# Legs [4][7] and [4][9]
-
-while play == True:
+'''
+Head [2][8]
+Torso [3][8]
+Arms [3][7] and [3][9]
+Legs [4][7] and [4][9]
+'''
+while True:
 
     #os.system('clear')
 
+    # prints the hanger
     for i in range(7):
         for j in range(10):
             print(hanger[i][j], end = ' ')
         print("")
 
-    for i in range(len(blank)):
-        print(blank[i], end = " ")
+    # prints the blanks line
+    for blank in blanks:
+        print(blank, end = " ")
     print("\n")
 
-    guess = input("Guess a letter:\n==> ").upper()
+    # prints the made guesses
+    for i in range(len(guesses)):
+        print("Guesses --> ", guesses[i], sep = '-', end = " ")
+    print("\n")
 
-    for i in range(len(letters)):
-        if blank == letters:
-            print("Congratulations, you won!")
-            play = False
-        elif letters[i] == guess:
-            blank[i] = guess
-        elif hanger[2][8] == 'O':
+    # checking secret word and guessed word
+    if blanks == letters:
+        print("Congratulations, you won!\n")
+        break
+    elif hanger[4][9] == '\\':
+        print("Sorry, you lost!\n")
+        break
+
+    # asks for a guess
+    guess = input("Guess a letter:\n==> ").upper()
+    guesses.append(guess)
+
+    # sweeps the letters list
+    if guess in letters:
+        for i in range(len(letters)):
+            if letters[i] == guess:
+                blanks[i] = guess
+    # loses 1 life
+    else:
+        if hanger[2][8] == 'O':
             hanger[2][8] = 'o'
         elif hanger[3][8] == ' ':
             hanger[3][8] = '|'
@@ -61,4 +81,3 @@ while play == True:
             hanger[4][7] = '/'
         elif hanger[4][9] == ' ':
             hanger[4][9] = '\\'
-            print("Sorry, you lost")
