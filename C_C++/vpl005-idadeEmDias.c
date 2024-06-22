870e92d3d4ed3709b93414d3a41a90333f02b135
Traian <traian@tutanota.com>
2024-06-22 05:05:04 -0300

    renaming remote in win10 station

--- C_C++/vpl005-idadeEmDias.c
+++ C_C++/vpl005-idadeEmDias.c
@@ -1,35 +1,24 @@
+/*
+IDADE EM DIAS
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+Entrada
+O arquivo de entrada contém um valor inteiro.
+Saída
+Imprima a saída conforme exemplo fornecido.
+Exemplos
+4001 ano(s)1 mes(es)5 dia(s)8002 ano(s)2 mes(es)10 dia(s)300 ano(s)1 mes(es)0 dia(s)
+*/
+
 #include <stdio.h>
 
 int main(void){
-
-    char tempo[4][6] = {"Anos", "Meses", "Dias"};
-    int dias;
-    int resto;
-    int total;
-
-    printf("Digite uma quantidade de dias: ");
-    scanf("%i", &dias);
-
-    for (int i = 0, j = 365; i < 3; i++, j -= 335){
-        if(j < 0){
-            total = resto;
-        }else{
-            resto = dias%j;
-            total = dias/j;
-            dias = resto;
-        }
-        printf("%s = %d\n", tempo[i], total);
-    }
-/* 
-    resto = dias%365;
-    total = dias/365;
-    dias = resto;
-    printf("Anos = %i\n", total);
-    resto %= dias/30;
-    total = dias/30;
-    printf("Meses = %i\n", total);
-    printf("Dias = %i\n", resto);
-*/
+    int dias, meses, anos;
+    scanf("%d", &dias);
+    anos = dias / 365;
+    dias %= 365;
+    meses = dias / 30;
+    dias %= 30;
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", anos, meses, dias);
     return 0;
-
 }
---
+++ C_C++/vpl006-gastoDeCombustivel.c
@@ -0,0 +1,33 @@
+/* 
+GASTO DE COMBUSTÍVEL
+Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+
+Entrada
+O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
+
+Saída
+Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
+
+Exemplos
+10
+85
+70.833
+
+2
+92
+15.333
+
+22
+67
+122.833
+*/
+
+#include <stdio.h>
+
+int main(){
+    double velocidade, espaco, tempo;
+    scanf("%lf %lf", &tempo, &velocidade);
+    espaco = velocidade / tempo;    
+    printf("%.3lf\n", espaco / 12.0);    
+    return 0;
+}
